ab987322bed05003e3cef2e86db02f684fc19500
Shivang <shivangmathur101@gmail.com>
2025-09-28 15:19:33 +0530

    Changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Hello From Amplify
         </p>
       </div>
       <p className="read-the-docs">
